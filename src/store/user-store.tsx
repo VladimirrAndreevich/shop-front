7e32a243f63af93bdbc8af90aece59c36ade7f07
VladimirrAndreevich <110813805+VladimirrAndreevich@users.noreply.github.com>
2024-05-12 14:40:44 +0400

    fix: add instance of store

--- src/store/user-store.tsx
+++ src/store/user-store.tsx
@@ -1,7 +1,9 @@
+import { I_CartItem } from "@/types";
 import { makeAutoObservable } from "mobx";
 
 class UserStore {
   isLogged = false;
+  cart?: I_CartItem[];
   token?: string;
 
   constructor() {
@@ -32,6 +34,41 @@ class UserStore {
     const formattedResponse = await response.json();
     console.log(formattedResponse);
   };
+
+  async addItemCart(productId: number, size: string) {
+    if (!this.isLogged) {
+      // throw new Error("Error access! Unauthorized.");
+      return;
+    }
+
+    const response = await fetch(
+      `${process.env.API_URL_BACKEND}/users/cart/add`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${this.token}`,
+        },
+        body: JSON.stringify({
+          productId,
+          createCartItemData: {
+            size,
+          },
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Error adding item to cart");
+    }
+  }
 }
 
-export default new UserStore();
+let userStore: UserStore;
+
+export const getStoreInstance = (): UserStore => {
+  if (!userStore) {
+    userStore = new UserStore();
+  }
+  return userStore;
+};
