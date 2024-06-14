3e3bd07fdff33c4ac0411071ac8f58ef627c5ab5
VladimirrAndreevich <110813805+VladimirrAndreevich@users.noreply.github.com>
2024-06-14 13:59:47 +0400

    fix: correct position logo

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -32,7 +32,7 @@ const Header: React.FC = observer(() => {
               <Box
                 display="flex"
                 justifyContent={{
-                  sx: "center",
+                  xs: "center",
                   sm: "start",
                 }}
               >
